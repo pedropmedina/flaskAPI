a6bc69dc97b578cc13bb16ef556779c9274705f5
pedropmedina <pedrodelilia@gmail.com>
2019-02-13 22:57:52 -0500

    Implement NotificationManager, NotifiacationList and Notification classes

--- service/service.py
+++ service/service.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import abor, Api, fields, marshal_with, reqparse, Resource
+from flask_restful import abort, Api, fields, marshal_with, reqparse, Resource
 from datetime import datetime
 from pytz import utc
 
@@ -11,18 +11,18 @@ class NotificationManager:
     last_id = 0
 
     def __init__(self):
-        self.notification = {}
+        self.notifications = {}
 
     def insert_notification(self, notification):
         self.__class__.last_id += 1
         notification.id = self.__class__.last_id
-        self.notification[self.__class__.last_id] = notification
+        self.notifications[self.__class__.last_id] = notification
 
     def get_notification(self, id):
-        return self.notification[id]
+        return self.notifications[id]
 
     def delete_notification(self, id):
-        del self.notification[id]
+        del self.notifications[id]
 
 
 notification_fields = {
@@ -37,3 +37,83 @@ notification_fields = {
 }
 
 notification_manager = NotificationManager()
+
+
+class Notification(Resource):
+    def abort_if_notification_not_found(self, id):
+        if id not in notification_manager.notifications:
+            abort(
+                HttpStatus.not_found_404.value,
+                message=f'Notification {id} doesn\'t exist.',
+            )
+
+    # marshal_with filters any other fields not provided in the
+    # notification_fields. Perfect to leave out any other fields
+    # of not interest comming along in the request
+    @marshal_with(notification_fields)
+    def get(self, id):
+        self.abort_if_notification_not_found(id)
+        return notification_manager.get_notification(id)
+
+    def delete(self, id):
+        self.abort_if_notification_not_found(id)
+        notification_manager.delete_notification(id)
+        return ('', HttpStatus.no_content_204.value)
+
+    @marshal_with(notification_fields)
+    def patch(self, id):
+        self.abort_if_notification_not_found(id)
+        notification = notification_manager.get_notification(id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('message', type=str)
+        parser.add_argument('ttl', type=int)
+        parser.add_argument('display_times', type=int)
+        parser.add_argument('diplay_once', type=bool)
+        args = parser.parse_args()
+        print(args)
+        if 'message' in args and args['message'] is not None:
+            notification.message = args['message']
+        if 'ttl' in args and args['ttl'] is not None:
+            notification.ttl = args['ttl']
+        if 'display_times' in args and args['display_times'] is not None:
+            notification.display_times = args['display_times']
+        if 'display_once' in args and args['display_once'] is not None:
+            notification.display_once = args['display_once']
+        return notification
+
+
+class NotificationList(Resource):
+    @marshal_with(notification_fields)
+    def get(self):
+        return [v for v in notification_manager.notifications.value()]
+
+    @marshal_with(notification_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'message',
+            type=str,
+            required=True,
+            help='Message cannot be left blank',
+        )
+        parser.add_argument(
+            'ttl',
+            type=int,
+            required=True,
+            help='Time to live cannot be left blank!',
+        )
+        parser.add_argument(
+            'notification_category',
+            type=str,
+            required=True,
+            help='Notification category cannot be blank!',
+        )
+        args = parser.parse_args()
+        notification = NotificationModel(
+            message=args['message'],
+            ttl=args['ttl'],
+            creation_date=datetime.now(utc),
+            notification_category=args['notification_category'],
+        )
+        notification_manager.insert_notification(notification)
+        return (notification, HttpStatus.created_201.value)
