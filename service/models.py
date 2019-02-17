4088dc5fdefbd77f34b7a9d55c33e78c94095af2
pedropmedina <pedrodelilia@gmail.com>
2019-02-16 22:50:25 -0500

    Add config file and made changes models file to user SQLAlchemy and Marshmallow

--- service/models.py
+++ service/models.py
@@ -1,10 +1,60 @@
-class NotificationModel:
-    def __init__(self, message, ttl, creation_date, notification_category):
-        # Automatically generate the new id
-        self.id = 0
+from marshmallow import Schema, fields, pre_load
+from marshmallow import validate
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+
+orm = SQLAlchemy()
+ma = Marshmallow()
+
+
+class ResourceAddUpdateDelete:
+    def add(self, resource):
+        orm.session.add(resource)
+        return orm.session.commit()
+
+    def update(self):
+        return orm.session.commit()
+
+    def delete(self, resource):
+        orm.session.delete(resource)
+        return orm.session.commit()
+
+
+class Notification(orm.Model, ResourceAddUpdateDelete):
+    id = orm.Column(orm.Integer, primary_key=True)
+    message = orm.Column(orm.String(250), unique=True, nullable=False)
+    ttl = orm.Column(orm.Integer, nullable=False)
+    creation_date = orm.Column(
+        orm.TIMESTAMP, server_default=orm.func.current_timestamp()
+    )
+    notification_category_id = orm.Column(
+        orm.Integer,
+        orm.ForeingKey('notification_category.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    notification_category = orm.relationship(
+        'NotificationCategory',
+        backref=orm.backref(
+            'notifications', lazy='dynamic', order_by='Notification.message'
+        ),
+    )
+    displayed_times = orm.Column(
+        orm.Integer, nullable=False, server_default='0'
+    )
+    displayed_once = orm.Column(
+        orm.Boolean, nullable=False, server_default='false'
+    )
+
+    def __init__(self, message, ttl, notification_category):
         self.message = message
         self.ttl = ttl
-        self.creation_date = creation_date
         self.notification_category = notification_category
-        self.display_times = 0
-        self.display_once = False
+
+
+class NotificationCategory(orm.Model, ResourceAddUpdateDelete):
+    id = orm.Column(orm.Integer, primary_key=True)
+    name = orm.Column(orm.String(150), unique=True, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
